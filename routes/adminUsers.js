9f1d9610e025411ffa888becb192443bcb1f0ebb
Anuradha <anudnz9@gmail.com>
2025-10-07 10:53:07 +1300

    adminUsers update and delete working

--- routes/adminUsers.js
+++ routes/adminUsers.js
@@ -6,19 +6,29 @@ const isAdmin = require("../middleware/auth");
 // Show all Users 
 router.get("/", isAdmin, (req, res) => {
     conn.query('SELECT * FROM users', (err, results) => {
-    if (err) throw err;
-    res.render('adminUsers', { users: results });
-  });
+        if (err) throw err;
+        res.render('adminUsers', { users: results });
+    });
 });
 
-// update ->"/update/:id", isAdmin,
+// Handle inline update form submission
+router.post("/update/:id", isAdmin, (req, res) => {
+    const userId = req.params.id;
+    const { username, email, phone, role } = req.body;
+
+    const sql = 'UPDATE users SET user_name = ?, email = ?, phone = ?, role = ? WHERE user_id = ?';
+    conn.query(sql, [username, email, phone, role, userId], (err) => {
+        if (err) throw err;
+        res.redirect('/admin/Users'); 
+    });
+});
 
 // Delete users
 router.get("/delete/:id", isAdmin, (req, res) => {
-  conn.query('DELETE FROM users WHERE id = ?', [req.params.id], (err) => {
-    if (err) throw err;
-    res.redirect('/admin/Users');
-  });
+    conn.query('DELETE FROM users WHERE user_id = ?', [req.params.id], (err) => {
+        if (err) throw err;
+        res.redirect('/admin/Users');
+    });
 });
 
 module.exports = router;
